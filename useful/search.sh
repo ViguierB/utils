47c0ddead68070df5ddd9bdfda7a112ea478c50c
Viguier Benjamin <benjamin.viguier@epitech.eu>
2017-10-10 23:12:53 +0200

    improve search.sh

--- useful/search.sh
+++ useful/search.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
+true=1;
+false=0;
+
+[ -t 1 ]
+isatty=$?;
+
+bold='1';
+default='0';
+
+function loadColor {
+    if [ -z $1 ] || ! $(exit $isatty); then
+	    return;
+    fi
+    res='\e[';
+    first=$true;
+    for color in "$@"; do
+	    if [ $first == $false ]; then
+	        res+=';';
+	    else
+	        first=$false;
+	    fi
+ 	    res+=$color;
+    done
+    res+='m';
+    echo -en "$res";
+}
+
 function getFiles {
     dirName=$1;
-    first=0;
-    if [ -z $2 ]; then
-       first=1;
-    fi
 
     whereiam=$(pwd);
     cd $dirName;
     for d in $(ls -a -I .. -I . -I .git); do
         if [ -d $d ]; then
-            echo $(getFiles $d true);
+            echo $(getFiles $d);
         else
-            if $(exit $first); then
-                echo "$(pwd)/$d";
-            fi;
+            echo "$(pwd)/$d";
         fi;
-    done
-    cd $whereiam
+    done;
+    cd $whereiam;
 }
 
 for a in $(getFiles $1); do
-    res=$(cat $a | grep --color=always -n $2)
+    res=$(cat $a | grep $(if $(exit $isatty); then echo '--color=always'; fi) -n ${@:2})
     if [ ! -z "$res" ]; then
-        echo "On file $a";
+        printf $(loadColor $bold)"Found on file: $a\n"$(loadColor $default)
         echo "$res";
         echo;
     fi
